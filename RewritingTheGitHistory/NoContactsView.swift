3c4040511cbf11673013124c72021753db86b719
Tiago Vargas <tiago.ce10@outlook.com>
2023-07-20 19:17:22 -0300

    Add `+` button to navigation view

--- RewritingTheGitHistory/NoContactsView.swift
+++ RewritingTheGitHistory/NoContactsView.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct NoContactsView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "person.fill")
-                .resizable()
-                .frame(width: 240, height: 240)
-                .scaledToFit()
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("No contacts yet")
-                .font(.largeTitle)
+        NavigationView {
+            VStack {
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .frame(width: 240, height: 240)
+                    .scaledToFit()
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("No contacts yet")
+                    .font(.largeTitle)
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.blue)
+                }
+            }
         }
-        .padding()
     }
 }
