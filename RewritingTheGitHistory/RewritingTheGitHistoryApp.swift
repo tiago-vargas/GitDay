3c30273876c90d19cf3d497d1510b42a2365bd19
Tiago Vargas <tiago.ce10@outlook.com>
2023-07-20 17:27:40 -0300

    Initial Commit

---
+++ RewritingTheGitHistory/RewritingTheGitHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewritingTheGitHistoryApp.swift
+//  RewritingTheGitHistory
+//
+//  Created by Tiago Vargas on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct RewritingTheGitHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
