24881b5930ab68785fda64e9bb4e365963691353
Tiago Vargas <tiago.ce10@outlook.com>
2023-07-20 19:16:09 -0300

    Add `NoContactsView`

--- RewritingTheGitHistory/NoContactsView.swift
+++ RewritingTheGitHistory/NoContactsView.swift
@@ -9,6 +9,16 @@ import SwiftUI
 
 struct NoContactsView: View {
     var body: some View {
-        Text("No Contacts - Stub")
+        VStack {
+            Image(systemName: "person.fill")
+                .resizable()
+                .frame(width: 240, height: 240)
+                .scaledToFit()
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("No contacts yet")
+                .font(.largeTitle)
+        }
+        .padding()
     }
 }
